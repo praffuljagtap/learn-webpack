8aedf410ad6778a0b196cc6e2e8afcfa7f7045e7
Prafful Jagtap <prafful.amexs@gmail.com>
2021-04-03 18:02:03 +1100

    Added Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './src/app.js')
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'deploy')
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack Output",
+        }),
+    ],
+};
